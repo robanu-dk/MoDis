4a4a35ef79c4d704ba3f550b746099eb3e0ca504
robanu <bestrobanu@gmail.com>
2024-05-19 03:57:39 +0700

    done: API for Activity features

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Activity;
 use App\Models\User;
+use App\Models\UserActivity;
+use App\Models\UserActivityCoordinate;
+use DateTime;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -40,6 +44,376 @@ class ActivityController extends Controller
             return response()->json([
                 'status' => 'success',
                 'data' => DB::select('SELECT * FROM user_activities ua INNER JOIN activities a ON ua.`id_activity` = a.`id` WHERE ua.`id_user` = ? AND a.`date` = ? ORDER BY a.`start_time` ASC', [$user->id, date('Y-m-d')]),
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $th->getMessage(),
+                'line' => $th->getLine(),
+                'file' => $th->getFile(),
+            ], 400);
+        }
+    }
+
+    public function getAllListActivity(Request $request)
+    {
+        try {
+            $user = User::where('email', $request->email)->first();
+
+            if (!$user){
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'gagal mendapatkan informasi',
+                ], 200);
+            }
+
+            if ($request->bearerToken() != $user->token || !$user->token) {
+                return response()->son([
+                    'status' => 'error',
+                    'message' => 'gagal mendapatkan informasi',
+                ], 200);
+            }
+
+            return response()->json([
+                'status' => 'success',
+                'data' => DB::select('SELECT a.*, ua.`done` FROM `activities` a INNER JOIN `user_activities` ua ON a.`id` = ua.`id_activity` WHERE ua.`id_user` = ? ORDER BY a.`date` DESC, a.`start_time` DESC', [$user->id]),
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $th->getMessage(),
+                'line' => $th->getLine(),
+                'file' => $th->getFile(),
+            ], 400);
+        }
+    }
+
+    public function getDetailActivity(Request $request)
+    {
+        try {
+            $user = User::where('email', $request->email)->first();
+
+            if (!$user){
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'gagal mendapatkan informasi kegiatan',
+                ], 200);
+            }
+
+            if ($request->bearerToken() != $user->token || !$user->token) {
+                return response()->son([
+                    'status' => 'error',
+                    'message' => 'gagal mendapatkan informasi kegiatan',
+                ], 200);
+            }
+
+            $activity = Activity::where('id', $request->activity_id)->first();
+
+            if (!$activity) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'gagal mendapatkan informasi kegiatan',
+                ], 200);
+            }
+
+            return response()->json([
+                'status' => 'success',
+                'data' => DB::select('SELECT a.*, ua.`id` AS `user_activity_id`, ua.`done`, uac.`id` AS `activity_coordinate_id`, u.`id` AS `user_id`, u.`name` FROM `activities` a INNER JOIN `user_activities` ua ON a.`id` = ua.`id_activity` LEFT JOIN `user_activity_coordinates` uac ON ua.`id` = uac.`id_user_activity` LEFT JOIN `users` u ON u.`id` = ua.`id_user` WHERE a.`id` = ? AND ua.`id_user` = ?', [$activity->id, $user->id])[0],
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $th->getMessage(),
+                'line' => $th->getLine(),
+                'file' => $th->getFile(),
+            ], 400);
+        }
+    }
+
+    public function getCoordinatesUserByGuide(Request $request)
+    {
+        try {
+            $user = User::where('email', $request->email)->first();
+
+            if (!$user) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'gagal mendapatkan koordinat',
+                ], 200);
+            }
+
+            if ($request->bearerToken() != $user->token || !$user->token || !$user->role || !$user->verified) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'gagal mendapatkan koordinat',
+                ], 200);
+            }
+
+            $user_activity = UserActivity::where('id_user', $request->child_id)->where('id_activity')->first();
+
+            if (!$user_activity) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'gagal mendapatkan koordinat',
+                ], 200);
+            }
+
+            return response()->json([
+                'status' => 'success',
+                'data' => UserActivityCoordinate::where('id_user_activity', $user_activity->id)->first(),
+            ], 200);
+        } catch (\THrowable $th) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $th->getMessage(),
+                'line' => $th->getLine(),
+                'file' => $th->getFile(),
+            ], 400);
+        }
+    }
+
+    public function createActivity(Request $request)
+    {
+        date_default_timezone_set('Asia/Jakarta');
+        try {
+            $user = User::where('email', $request->email)->first();
+
+            if (!$user){
+                return response()->json([
+                    'status' => 'error',
+                    'message' => '',
+                ], 200);
+            }
+
+            if ($request->bearerToken() != $user->token || !$user->token) {
+                return response()->son([
+                    'status' => 'error',
+                    'message' =>'',
+                ], 200);
+            }
+
+            $activity = new Activity();
+            $activity->name = $request->activity_name;
+            $activity->date = $request->activity_date;
+            $activity->start_time = $request->activity_start_time;
+            $activity->end_time = $request->activity_end_time;
+            $activity->note = $request->activity_note;
+            $activity->created_by_guide = $request->child_account_id && $user->role;
+            $activity->created_at = now();
+            $activity->updated_at = now();
+            $activity->save();
+
+            UserActivity::create([
+                'id_user' => $user->id,
+                'id_activity' => $activity->id,
+            ]);
+
+            if ($request->child_account_id) {
+                UserActivity::create([
+                    'id_user' => $request->child_accoun_id,
+                    'id_activity' => $activity->id,
+                ]);
+            }
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'berhasil menambahkan kegiatan',
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $th->getMessage(),
+                'line' => $th->getLine(),
+                'file' => $th->getFile(),
+            ], 400);
+        }
+    }
+
+    public function updateActivity(Request $request)
+    {
+        date_default_timezone_set('Asia/Jakarta');
+        try {
+            $user = User::where('email', $request->email)->first();
+
+            if (!$user){
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'gagal memperbarui kegiatan',
+                ], 200);
+            }
+
+            if ($request->bearerToken() != $user->token || !$user->token) {
+                return response()->son([
+                    'status' => 'error',
+                    'message' => 'gagal memperbarui kegiatan',
+                ], 200);
+            }
+
+            $activity = Activity::where('id', $request->activity_id)->first();
+
+            if (!$activity) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'gagal memperbarui kegiatan',
+                ], 200);
+            }
+
+            if ($activity->created_by_guide && !$user->role) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'gagal memperbarui kegiatan',
+                ], 200);
+            }
+
+            $activity->update([
+                'name' => $request->event_name,
+                'date' => $request->date,
+                'start_time' => $request->start_time,
+                'end_time' => $request->end_time,
+                'note' => $request->note,
+            ]);
+
+            if ($user->role && $request->child_account_id) {
+                $list_child_id = implode('\',\'', $request->child_account_id);
+                DB::delete('DELETE FROM `user_activities` ua WHERE ua.`id_user` NOT IN ?', [$list_child_id]);
+
+                foreach($request->child_account_id as $child_id) {
+                    UserActivity::create([
+                        'id_user' => $child_id,
+                        'id_activity' => $activity->id,
+                    ]);
+                }
+            }
+            return response()->json([
+                'status' => 'success',
+                'message' => 'berhasil memperbarui kegiatan',
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $th->getMessage(),
+                'line' => $th->getLine(),
+                'file' => $th->getFile(),
+            ], 400);
+        }
+    }
+
+    public function deleteActivity(Request $request)
+    {
+        date_default_timezone_set('Asia/Jakarta');
+        try {
+            $user = User::where('email', $request->email)->first();
+
+            if (!$user){
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'gagal menghapus kegiatan',
+                ], 200);
+            }
+
+            if ($request->bearerToken() != $user->token || !$user->token) {
+                return response()->son([
+                    'status' => 'error',
+                    'message' => 'gagal menghapus kegiatan',
+                ], 200);
+            }
+
+            $activity = Activity::where('id', $request->activity_id)->first();
+
+            if (!$activity) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'gagal menghapus kegiatan',
+                ], 200);
+            }
+
+            $date_event = new DateTime($activity->date . ' ' . $activity->start_time);
+            $date_now = new DateTime();
+
+            if ($date_now > $date_event) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'gagal menghapus kegiatan',
+                ], 200);
+            }
+
+            if ($activity->created_by_guide && !$user->role) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'gagal menghapus kegiatan',
+                ], 200);
+            }
+
+            if ($user->role) {
+                DB::delete('DELETE FROM `user_activities` ua WHERE ua.`id_activity` = ?', [$activity->id]);
+            } else {
+                DB::delete('DELETE FROM `user_activities` ua WHERE ua.`id_user` = ? AND ua.`id_activity` = ?', [$user->id, $activity->id]);
+            }
+
+            $activity->delete();
+
+            return response()->json([
+                'status' => 'error',
+                'message' => 'berhasil menghapus kegiatan',
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $th->getMessage(),
+                'line' => $th->getLine(),
+                'file' => $th->getFile(),
+            ], 400);
+        }
+    }
+
+    public function finishActivity(Request $request)
+    {
+        try {
+            $user = User::where('email', $request->email)->first();
+
+            if (!$user){
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'gagal menyelesaikan kegiatan',
+                ], 200);
+            }
+
+            if ($request->bearerToken() != $user->token || !$user->token) {
+                return response()->son([
+                    'status' => 'error',
+                    'message' => 'gagal menyelesaikan kegiatan',
+                ], 200);
+            }
+
+            $user_activities = UserActivity::where('id', $request->activity_id)->get();
+
+            if (!count($user_activities)) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'gagal menyelesaikan kegiatan',
+                ], 200);
+            }
+
+            foreach ($user_activities as $activity) {
+                if (!$activity->done) {
+                    $activity->update([
+                        'done' => 1,
+                    ]);
+
+                    if ($request->coordinates_tracking) {
+                        foreach($request->coordinates_tracking as $coordinate) {
+                            UserActivityCoordinate::create([
+                                'id_user_activity' => $activity->id,
+                                'coordinate' => $coordinate,
+                            ]);
+                        }
+                    }
+                }
+            }
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'berhasil menyelesaikan event',
             ], 200);
         } catch (\Throwable $th) {
             return response()->json([
