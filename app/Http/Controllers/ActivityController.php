a5efb82a008870cd50929b3c5f6c4e08b3ae55b4
robanu <bestrobanu@gmail.com>
2024-05-20 01:04:39 +0700

    add: column start_time and finishing_time to record

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -118,7 +118,7 @@ class ActivityController extends Controller
 
             return response()->json([
                 'status' => 'success',
-                'data' => DB::select('SELECT a.*, ua.`id` AS `user_activity_id`, ua.`done`, uac.`id` AS `activity_coordinate_id`, u.`id` AS `user_id`, u.`name` FROM `activities` a INNER JOIN `user_activities` ua ON a.`id` = ua.`id_activity` LEFT JOIN `user_activity_coordinates` uac ON ua.`id` = uac.`id_user_activity` LEFT JOIN `users` u ON u.`id` = ua.`id_user` WHERE a.`id` = ? AND ua.`id_user` = ?', [$activity->id, $user->id])[0],
+                'data' => DB::select('SELECT a.*, ua.`id` AS `user_activity_id`, ua.`done` ua.`start_time` AS `start_activity_time`, ua.`finishing_time` AS `finishing_activity_time`, uac.`id` AS `activity_coordinate_id`, u.`id` AS `user_id`, u.`name` FROM `activities` a INNER JOIN `user_activities` ua ON a.`id` = ua.`id_activity` LEFT JOIN `user_activity_coordinates` uac ON ua.`id` = uac.`id_user_activity` LEFT JOIN `users` u ON u.`id` = ua.`id_user` WHERE a.`id` = ? AND ua.`id_user` = ?', [$activity->id, $user->id])[0],
             ], 200);
         } catch (\Throwable $th) {
             return response()->json([
@@ -397,6 +397,8 @@ class ActivityController extends Controller
             foreach ($user_activities as $activity) {
                 if (!$activity->done) {
                     $activity->update([
+                        'start_time' => $request->start_time,
+                        'finishing_time' => $request->finishing_time,
                         'done' => 1,
                     ]);
 
